512988e2e8cfeca039735b14e3d849fc1440cdeb
Gautham Durairaj <gautham.durairaj@sky.uk>
2022-05-27 17:05:37 +0000

    RDKB-42484 : License Issues in rdk-wanmanager
    Reason for change: remove unwanted license in code
    Test Procedure:
    	* should have no functional impact
    
    Change-Id: Ifcd162bc0e09bbe99b140edcc9d0d71663ddcd15
    Risks: Low
    Signed-off-by: gautham.durairaj@sky.uk
    (cherry picked from commit 1f5d35e437148d0fc3d4ae3c451b5b4812f41428)
    (cherry picked from commit 5238ffd0ca52b214912d35a53882d6f0e564706d)

--- source/TR-181/middle_layer_src/wanmgr_rbus_handler_apis.c
+++ source/TR-181/middle_layer_src/wanmgr_rbus_handler_apis.c
@@ -16,21 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-/*
- * Copyright [2014] [Cisco Systems, Inc.]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     LICENSE-2.0" target="_blank" rel="nofollow">http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 #ifdef RBUS_BUILD_FLAG_ENABLE
 #include <rbus.h>
 #include "wanmgr_data.h"
