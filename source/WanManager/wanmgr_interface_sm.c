e1ca2dde782d5cc23f45a340a66d1cc9728b2d36
Abhishek Sivappa <abhishek.sivappa@sky.uk>
2023-01-11 15:18:17 +0000

    SKYH4-6650: REVSSH is not working for latest 22Q4 sprint PROD builds
    Reason for change: Prod builds require ssh start once interface
    	           and wan gets configured.
    
    Test Procedure:
            1. Reverse ssh should work fine in prod and dev builds.
    
    Priority: P0
    Risks: Low
    
    Change-Id: I056c0f8b89514114fdbdad4d659b67736b9d2266
    Signed-off-by: Abhishek Sivappa <abhishek.sivappa@sky.uk>
    (cherry picked from commit 389e8a19ee5fa6f3849bd6277f32be9208edeaa7)

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -845,6 +845,7 @@ static int wan_setUpIPv4(WanMgr_IfaceSM_Controller_t * pWanIfaceCtrl)
         {
             syscfg_set_string(SYSCFG_WAN_INTERFACE_NAME, pInterface->IP.Ipv4Data.ifname);
         }
+        wanmgr_sshd_restart();
         //Get WAN uptime
         WanManager_GetDateAndUptime( buffer, &uptime );
         LOG_CONSOLE("%s [tid=%ld] v4: Wan_init_complete for interface index %d at %d\n", buffer, syscall(SYS_gettid), pWanIfaceCtrl->interfaceIdx, uptime);
@@ -984,6 +985,7 @@ static int wan_setUpIPv6(WanMgr_IfaceSM_Controller_t * pWanIfaceCtrl)
         {
             syscfg_set_string(SYSCFG_WAN_INTERFACE_NAME, pInterface->IP.Ipv6Data.ifname);
         }
+        wanmgr_sshd_restart();
     }
 
     return ret;
