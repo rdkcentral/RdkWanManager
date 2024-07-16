8a6345d05eb6cc8e52ae398a78afc2337d80edd6
parthiban.selvaraj <parthiban.selvaraj@sky.uk>
2024-07-16 14:26:24 +0000

    XYZ-999 : ConfigurableWanName for XB
    Reason for change:  Solving Build errors
    
    Test Procedure:
        Updated in Jira.
    
    Risks: none
    Priority: P1
    
    Signed-off-by: parthiban.selvaraj <parthiban.selvaraj@sky.uk>

--- source/TR-181/middle_layer_src/wanmgr_rdkbus_apis.c
+++ source/TR-181/middle_layer_src/wanmgr_rdkbus_apis.c
@@ -266,7 +266,8 @@ int get_Virtual_Interface_FromPSM(ULONG instancenum, ULONG virtInsNum ,DML_VIRTU
         pVirtIf->Enable = TRUE;
     }
 
-#if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE) /* TODO: This is a workaround for the platforms using same Wan Name.*/
+//#if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE) /* TODO: This is a workaround for the platforms using same Wan Name.*/
+#if  !(defined (_XB6_PRODUCT_REQ_) || defined (_CBR2_PRODUCT_REQ_))
     _ansc_memset(param_name, 0, sizeof(param_name));
     _ansc_memset(param_value, 0, sizeof(param_value));
     _ansc_sprintf(param_name, PSM_WANMANAGER_IF_VIRIF_NAME, instancenum, (virtInsNum + 1));
