7eacd6cf5f98abc8a1de4babcc7c83b6dd409ed5
Lakshminarayanan <lakshminarayanan.shenbagaraj2@sky.uk>
2024-12-13 14:32:35 +0000

    RDKB-57439 : [XER10-UK] IPv6 Handling - Wan & Lan Management. Reason for change: IPv6 Handling Requirement for Sky Products are different from Comcast. Sky using IAPD , DHCPv6 prefix delegation to provide prefixes for use on the subscriber LAN as well as to get WAN IPv6 address from backend.
    The implementation for this feature is under CcspPandM components so that needs to be enable for applicable products like Sky. So we need to control RDKB_GLOBAL_PRODUCT_REQ common MACRO with feature applicable flags with respect to region.
    
    Test Procedure:
    1. IPv6 ULA should work for Sky but those params writable permission should be prohibited for comcast products.
    2. WAN and LAN functionality should work without any issue.
    
    Risks: Medium
    
    Signed-off-by: Lakshminarayanan <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/WanManager/wanmgr_net_utils.c
+++ source/WanManager/wanmgr_net_utils.c
@@ -488,11 +488,16 @@ int WanManager_Ipv6AddrUtil(char *ifname, Ipv6OperType opr, int preflft, int val
         {
             if (strlen(prefix) > 0)
             {
-#if !(defined (_XB6_PRODUCT_REQ_) || defined (_CBR2_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_))  //Do not delete prefix from LAn bridge for the comcast platforms.
-                memset(cmdLine, 0, sizeof(cmdLine));
-                snprintf(cmdLine, sizeof(cmdLine), "ip -6 addr del %s/64 dev %s", prefixAddr, IfaceName);
-                if (WanManager_DoSystemActionWithStatus("ip -6 addr del ADDR dev xxxx", cmdLine) != 0)
-                    CcspTraceError(("failed to run cmd: %s", cmdLine));
+#if !(defined (_XB6_PRODUCT_REQ_) || defined (_CBR2_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_)) || defined(_RDKB_GLOBAL_PRODUCT_REQ_) //Do not delete prefix from LAn bridge for the comcast platforms.
+#if defined(_RDKB_GLOBAL_PRODUCT_REQ_)
+                if ( TRUE == WanMgr_Util_IsThisFeatureApplicable(SYSEVENT_FEATURE_IPv6_PREFIX_ON_LANBRIDGE_SUPPORT, INPUT_SOURCE_TYPE_SYSEVENT) )
+#endif /** _RDKB_GLOBAL_PRODUCT_REQ_ */
+                {
+                    memset(cmdLine, 0, sizeof(cmdLine));
+                    snprintf(cmdLine, sizeof(cmdLine), "ip -6 addr del %s/64 dev %s", prefixAddr, IfaceName);
+                    if (WanManager_DoSystemActionWithStatus("ip -6 addr del ADDR dev xxxx", cmdLine) != 0)
+                        CcspTraceError(("failed to run cmd: %s", cmdLine));
+                }
 #endif
                 memset(cmdLine, 0, sizeof(cmdLine));
 #if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE)
