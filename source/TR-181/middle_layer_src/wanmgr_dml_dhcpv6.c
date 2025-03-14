ec6665afa759bcc963ecc392b415972196968fff
LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>
2025-03-14 15:45:36 +0000

    XER10-1052 : MapTransportMode param value is empty after reboot.
    Reason for change:
    Default("NONE" display before selecting MAP-T mode) DML handling for MapTransportMode parameter is missing for FEATURE_MAPT use case so this needs to be done.
    
    Test Procedure:
    1. WAN should work without any issue
    
    Risks: Medium
    
    Signed-off-by: LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/TR-181/middle_layer_src/wanmgr_dml_dhcpv6.c
+++ source/TR-181/middle_layer_src/wanmgr_dml_dhcpv6.c
@@ -3483,7 +3483,7 @@ dhcp6c_mapt_mape_GetParamStringValue
         if ( AnscSizeOfString(temp) < *pUlSize)
         {
             AnscCopyString(pValue, temp);
-#if defined (FEATURE_SUPPORT_MAPT_NAT46)
+#if defined (FEATURE_SUPPORT_MAPT_NAT46) || defined(FEATURE_MAPT)
             if ( !(*temp) )
             {
                  AnscCopyString(pValue, "NONE");
