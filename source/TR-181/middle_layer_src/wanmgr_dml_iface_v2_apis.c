1630dcc5a87b0f5325b844285d5455f0ff65e005
kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>
2025-06-10 07:12:09 +0100

    Code changes for telemetry
    Signed-off-by: kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>

--- source/TR-181/middle_layer_src/wanmgr_dml_iface_v2_apis.c
+++ source/TR-181/middle_layer_src/wanmgr_dml_iface_v2_apis.c
@@ -2118,7 +2118,7 @@ BOOL WanIfIpCfg_SetParamUlongValue(ANSC_HANDLE hInsContext, char* ParamName, ULO
                 //Telemetry start
                 WanMgr_Telemetry_Marker_t Marker = {0};
                 Marker.enTelemetryMarkerID = WAN_INFO_IP_MODE;
-                Marker.pVirtInterface = pVirtIf;
+                Marker.pVirtInterface = p_VirtIf;
                 wanmgr_telemetry_event(&Marker);
                 //Telemetry end		
             }
