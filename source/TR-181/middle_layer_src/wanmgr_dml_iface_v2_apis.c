491e41876b750b162903d20c5a24b0f481019873
kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>
2025-06-11 10:43:08 +0100

    Code changes for telemetry
    Signed-off-by: kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>

--- source/TR-181/middle_layer_src/wanmgr_dml_iface_v2_apis.c
+++ source/TR-181/middle_layer_src/wanmgr_dml_iface_v2_apis.c
@@ -114,7 +114,7 @@ ULONG WanIf_GetEntryCount(ANSC_HANDLE hInsContext)
 ANSC_HANDLE WanIf_GetEntry(ANSC_HANDLE hInsContext, ULONG nIndex, ULONG* pInsNumber)
 {
     ANSC_HANDLE pDmlEntry = NULL;
-WanIf_GetEntryCcspTraceInfo(("%s:%d KAVYA_CRASH nIndex =[%lu] \n",__FUNCTION__,__LINE__,nIndex));
+CcspTraceInfo(("%s:%d KAVYA_CRASH nIndex =[%lu] \n",__FUNCTION__,__LINE__,nIndex));
     WanMgr_Iface_Data_t* pWanDmlIfaceData = WanMgr_GetIfaceData_locked(nIndex);
     CcspTraceInfo(("%s:%d KAVYA_CRASH \n",__FUNCTION__,__LINE__));
     if(pWanDmlIfaceData != NULL)
