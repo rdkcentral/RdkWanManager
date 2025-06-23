9a6fd8fde0a8cca8f29ffa88801a0d52cc4c5aab
kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>
2025-06-23 15:12:29 +0100

    RDKB-58521 : WAN Manager Telemetry Markers 2.0 - Phase 1
    Reason for change: Implementing Telemetry support for Wanmanager.
    Scalable to handle further telemetry versions
    
    Test Procedure:
    Updated in Jira.
    
    Risks: none
    Priority: P1
    
    Signed-off-by: kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>

--- source/TR-181/middle_layer_src/wanmgr_dml_iface_apis.c
+++ source/TR-181/middle_layer_src/wanmgr_dml_iface_apis.c
@@ -43,6 +43,7 @@
 #if defined (_XB6_PRODUCT_REQ_) || defined (_CBR2_PRODUCT_REQ_) || defined(_PLATFORM_RASPBERRYPI_)
 #include "wanmgr_utils.h"
 #endif
+
 extern WANMGR_DATA_ST gWanMgrDataBase;
 /***********************************************************************
 
