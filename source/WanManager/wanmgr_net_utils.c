069c3089576ff101896540703909cd2da4b8cb67
LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>
2025-07-14 12:27:48 +0100

    RDKB-60436 : Develop Code.
    Reason for change:
    RDKB-59763 - Develop that US acceptance criteria.
    
    Test Procedure:
    1. Build should passed
    2. WANManager functionality should work without any issue
    
    Risks: Medium
    
    Signed-off-by: LakshminarayananShenbagaraj <lakshminarayanan.shenbagaraj2@sky.uk>

--- source/WanManager/wanmgr_net_utils.c
+++ source/WanManager/wanmgr_net_utils.c
@@ -600,14 +600,17 @@ int WanManager_StartDhcpv4Client(DML_VIRTUAL_IFACE* pVirtIf, char* baseInterface
         return 0;
     }
 
-    char cmdInterfaceAction[128] = {0};
-
-    //This is temporary HACK to make the virtual interface Up
-    snprintf(cmdInterfaceAction, sizeof(cmdInterfaceAction), "ip link set dev %s up", pVirtIf->Name);
-    if (0 != WanManager_DoSystemActionWithStatus("start_dhcpc", cmdInterfaceAction))
+    //This is temporary HACK to make the virtual interface Up. This needs to be removed when WiFi side changes done
+    if ( 0 == strncmp(pVirtIf->Alias, HOTSPOT_ALIAS_NAME, strlen(HOTSPOT_ALIAS_NAME) ) )
     {
-        CcspTraceError(("Failed to run: %s:%d", cmdInterfaceAction, ret));
-        return -1;
+        char cmdInterfaceAction[128] = {0};
+
+        snprintf(cmdInterfaceAction, sizeof(cmdInterfaceAction), "ip link set dev %s up", pVirtIf->Name);
+        if (0 != WanManager_DoSystemActionWithStatus("start_dhcpc", cmdInterfaceAction))
+        {
+            CcspTraceError(("Failed to run: %s:%d", cmdInterfaceAction, ret));
+            return -1;
+        }
     }
 
 #if  defined( FEATURE_RDKB_DHCP_MANAGER )
