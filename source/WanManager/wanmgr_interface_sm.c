4803810971a8012f497b9907f41cce7757e7f840
kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>
2025-07-23 11:45:07 +0100

    RDKB-58521 : WAN Manager Telemetry Markers 2.0 - Phase 1
    Reason for change: Implementing Telemetry support for Wanmanager.
    Scalable to handle further telemetry versions
    
    Test Procedure:
    Updated in Jira.
    
    Risks: none
    Priority: P1
    
    Signed-off-by: kavya.shivalingaiah@sky.uk <kavya.shivalingaiah@sky.uk>

--- source/WanManager/wanmgr_interface_sm.c
+++ source/WanManager/wanmgr_interface_sm.c
@@ -4041,12 +4041,6 @@ static ANSC_STATUS WanMgr_IfaceIpcMsg_handle(WanMgr_IfaceSM_Controller_t* pWanIf
 
     if (p_VirtIf->IP.pIpcIpv4Data != NULL )
     {
-        static doOnce = 1;
-        if (doOnce)
-	{
-            p_VirtIf->IP.pIpcIpv4Data->isExpired = FALSE;
-            doOnce = 0;
-	}
         wanmgr_handle_dhcpv4_event_data(p_VirtIf);
     }
 
